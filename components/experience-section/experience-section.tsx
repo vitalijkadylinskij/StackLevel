573bd0bea8b82bce65857dd90435728b1154b18b
vitalijkadylinskij <vitalijkadylinskij@gmail.com>
2025-11-17 12:00:20 +0300

    fix style visible

--- components/experience-section/experience-section.tsx
+++ components/experience-section/experience-section.tsx
@@ -12,7 +12,7 @@ export function ExperienceSection() {
                 <div
                     className="rounded-xl w-full h-full min-h-[780px] bg-[url('/images/exp-section.jpg')] bg-cover bg-no-repeat bg-center">
                     <div className="flex flex-col h-[780px] justify-between py-20 px-15">
-                        <div className="flex w-full h-full">
+                        <div className="flex w-full h-full justify-between">
                             <h2 className="font-normal text-5xl max-w-[525px]">
                                 Build Smarter with AI Generators
                             </h2>
@@ -22,7 +22,7 @@ export function ExperienceSection() {
                                     AI for Everyone
                                 </div>
                                 <div
-                                    className="w-[310px] h-[27px] rounded-md mr-5 bg-white/17 text-center items-center">Creations
+                                    className="w-[310px] h-[27px] rounded-md bg-white/17 text-center items-center">Creations
                                     with AI-Driven Technology
                                 </div>
                             </div>
